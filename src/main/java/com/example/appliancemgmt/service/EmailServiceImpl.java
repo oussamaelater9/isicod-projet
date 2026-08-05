8a653bc19e22ff10f476e20dafc89f2fcc7f78e2
oussamaelater9 <elateroussama9@gmail.com>
2026-08-05 20:06:10 +0100

    Modified prod and added resend

--- src/main/java/com/example/appliancemgmt/service/EmailServiceImpl.java
+++ src/main/java/com/example/appliancemgmt/service/EmailServiceImpl.java
@@ -1,60 +1,67 @@
 package com.example.appliancemgmt.service;
 
+import com.resend.Resend;
+import com.resend.services.emails.model.CreateEmailOptions;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 @Service
 public class EmailServiceImpl implements EmailService {
 
-    @Autowired
-    private JavaMailSender mailSender;
     private static final Logger log =
             LoggerFactory.getLogger(EmailServiceImpl.class);
 
-    @Value("${frontend.url:http://localhost:4200}")
+    @Value("${resend.api.key}")
+    private String apiKey;
+
+    @Value("${resend.from}")
+    private String from;
+
+    @Value("${frontend.url}")
     private String frontendUrl;
 
     @Override
     public void sendPasswordResetEmail(String to, String token) {
 
-        String resetLink = frontendUrl + "/reset-password?token=" + token;
-
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        message.setTo(to);
-        message.setSubject("Reset your Appliance Management password");
-
-        message.setText("""
-Hello,
-
-We received a request to reset your password.
-
-Click the link below:
-
-%s
-
-This link expires in 30 minutes.
-
-If you didn't request a password reset, simply ignore this email.
-
-Regards,
-Appliance Management Team
-""".formatted(resetLink));
-
         try {
-            mailSender.send(message);
-            log.info("MAIL SENT");
+
+            String resetLink =
+                    frontendUrl + "/reset-password?token=" + token;
+
+            Resend resend = new Resend(apiKey);
+
+            CreateEmailOptions params = CreateEmailOptions.builder()
+                    .from(from)
+                    .to(to)
+                    .subject("Reset your Appliance Management password")
+                    .html("""
+                            <h2>Password Reset</h2>
+
+                            <p>We received a request to reset your password.</p>
+
+                            <p>
+                                <a href="%s">
+                                    Reset Password
+                                </a>
+                            </p>
+
+                            <p>This link expires in 30 minutes.</p>
+
+                            <p>If you didn't request this, simply ignore this email.</p>
+                            """.formatted(resetLink))
+                    .build();
+
+            resend.emails().send(params);
+
+            log.info("Password reset email sent to {}", to);
+
         } catch (Exception e) {
-            e.printStackTrace();
-            log.error("MAIL ERROR", e);
-            throw new RuntimeException(e);
+
+            log.error("Failed to send email", e);
+
+            throw new RuntimeException("Unable to send email", e);
         }
     }
 }
